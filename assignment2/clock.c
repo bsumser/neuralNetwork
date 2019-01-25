f2347fcf3d9bce66710844da1227061994814d8c
bsumserLaptop <bsumser@uoregon.edu>
2019-01-25 13:56:04 -0800

    added manipulation of time as array

--- assignment2/clock.c
+++ assignment2/clock.c
@@ -5,6 +5,12 @@
 #include <stdbool.h>
 
 #define BUF_LEN 256
+#define HEIGHT 8
+#define WIDTH 30 
+
+void printClock() {
+	char clock[HEIGHT][WIDTH];
+}
 
 int main() {
 	char buf[BUF_LEN] = {0};
@@ -13,5 +19,11 @@ int main() {
 	struct tm *ptm = localtime(&rawtime);
 
 	strftime(buf, BUF_LEN, "%R", ptm);
+	
+	//char timeString[5];
+	char timeString[5] = {buf[0],buf[1],buf[2],buf[3],buf[4]};
+
 	puts(buf);
+
+	printf("%c%c%c%c%c", timeString[0],timeString[1],timeString[2],timeString[3],timeString[4]);
 }
