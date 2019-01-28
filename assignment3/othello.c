e43d148181b9ae3f8b308c87c2fbf6c279d9e369
bsumserLaptop <bsumser@uoregon.edu>
2019-01-28 15:11:31 -0800

    comment for clarity

--- assignment3/othello.c
+++ assignment3/othello.c
@@ -4,7 +4,7 @@
 typedef enum state {White, Black, Full} State; //define enum datatype for each square on the board
 
 typedef struct board { //struct contain the board, dynamically allocated array of State
-	int boardSize = 12;
+	int boardSize = 12; //base board size for game
 	State* boardArray = (State*)malloc(boardSize * boardSize * sizeof(State));
 }gameBoard;
 
