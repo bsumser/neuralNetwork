5156e35860ad2dac42ddffcb80eda76313a12ec8
bsumserLaptop <bsumser@uoregon.edu>
2019-01-28 15:01:27 -0800

    working on representing board as array of enums

---
+++ assignment3/othello.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef enum state {White, Black, Full} State; //define enum datatype for each square on the board
+
+typedef struct board { //struct contain the board, dynamically allocated array of State
+	//State *boardArray[][];
+	int boardSize = 12;
+	State* boardArray = (State*)malloc(boardSize * boardSize * sizeof(State));
+}gameBoard;
+
+int main() {
+	int board = 0;	
+	printf("welcome to othello, please enter the size of the board\n");
+	scanf("%d", &board);
+	
+	return 0;
+}
