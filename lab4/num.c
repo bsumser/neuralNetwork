6a27853987239a5ab9311d570d5ad528263ec956
bsumserLaptop <bsumser@uoregon.edu>
2019-02-01 14:33:57 -0800

    added for additional for loop to hit every index, and change i<= size to i < size in memory deallocation

--- lab4/num.c
+++ lab4/num.c
@@ -6,13 +6,15 @@ int main(int argc, char **argv) {
 	int size = 5, i, j;
 
 	//Allocate memory for 2-D int array
-	num = (int **) malloc (size * sizeof(int *));
+	num = (int **)malloc(size * sizeof(int *));
  	for (i = 0; i < size; i++)
-		num[i] = (int *) malloc (size * sizeof(int *));
+		num[i] = (int *)malloc(size * sizeof(int));
 
     // Set values
-   	for (j = 0; j < size; j++)
-		num[i][j] = i * size + j;
+	for (i = 0; i < size; i++) {
+   		for (j = 0; j < size; j++)
+			num[i][j] = i * size + j;
+	}
 
 	// Print values
  	for (i = 0; i < size; i++) {
@@ -23,7 +25,7 @@ int main(int argc, char **argv) {
 	}
 
 	// Deallocate memory
-	for (i = 0; i <= size; i++)
+	for (i = 0; i < size; i++)
 		free(num[i]);
 				        																				free(num);
 }
