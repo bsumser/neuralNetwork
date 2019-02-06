7d42e62df2fedc95320a34b1feb7856ad43d07ca
bsumserLaptop <bsumser@uoregon.edu>
2019-02-06 14:36:04 -0800

    added do while for game loop, infinite loop on bad input

--- assignment4/main.cpp
+++ assignment4/main.cpp
@@ -1,63 +1,79 @@
 #include <iostream>
 #include <stdlib.h>
 
-using namespace std;
-
 class Board
 {
-	char board[8][8] =((' ', ' ', ' ',' ',' ',' ',' ',' ',), 
-	                   (' ', ' ', ' ',' ',' ',' ',' ',' ',), 
-	                   (' ', ' ', ' ',' ',' ',' ',' ',' ',), 
-	                   (' ', ' ', ' ',' ',' ',' ',' ',' ',), 
-	                   (' ', ' ', ' ',' ',' ',' ',' ',' ',), 
-	                   (' ', ' ', ' ',' ',' ',' ',' ',' ',), 
-	                   (' ', ' ', ' ',' ',' ',' ',' ',' ',), 
-	                   (' ', ' ', ' ',' ',' ',' ',' ',' ',), 
-	                   (' ', ' ', ' ',' ',' ',' ',' ',' ',)); 
+	char board[8][8] = {(' ', ' ', ' ',' ',' ',' ',' ',' '), 
+	                   (' ', ' ', ' ',' ',' ',' ',' ',' '), 
+	                   (' ', ' ', ' ',' ',' ',' ',' ',' '), 
+	                   (' ', ' ', ' ',' ',' ',' ',' ',' '), 
+	                   (' ', ' ', ' ',' ',' ',' ',' ',' '), 
+	                   (' ', ' ', ' ',' ',' ',' ',' ',' '), 
+	                   (' ', ' ', ' ',' ',' ',' ',' ',' '), 
+	                   (' ', ' ', ' ',' ',' ',' ',' ',' '), 
+	                   (' ', ' ', ' ',' ',' ',' ',' ',' ')}; 
 					     
 	int row = 1;
 	int column = 1;
 
+};
+
+void displayBoard();
+void getMove();
+bool checkWin();
+bool checkLegal();
+
+
+int main()
+{
+	//basic game loop
+	do {
+	
+			//display the board
+			displayBoard();
+			
+			//get a move
+			getMove();
+
+			//check if legal
+			checkLegal();
+
+			//checking for a win
+			checkWin();
+	}while(true);
+		
+	return 0;
 }
 
 void displayBoard()
 {
-	cout << "checking if the move is legal" << endl;
+		std::cout << "displaying the board" << std::endl;
 }
 
 void getMove()
 {
-		cout << "Please input your move in format row then column" << endl;
-		cin << row << col;
+	int userRow;
+	int userCol;
 
-		//change row and col to array notation
-		row = row - 1;
-		col = col - 1;
+	std::cout << "Please input your move in format row then column" << std::endl;
+	std::cin >> userRow >> userCol;
+
+	//loop until it recieves proper input
+
+	//change row and col to array notation
+	userRow = userRow - 1;
+	userCol = userCol - 1;
 
 }
 bool checkWin()
 {
-	cout << "checking for win" << endl;
+	std::cout << "checking for win" << std::endl;
+	return true;
 }
 
 bool checkLegal()
 {
 
-	cout << "checking if move is legal" << endl;
-}
-
-int main()
-{
-	//basic game loop
-	
-	//display the board
-	displayBoard();
-	
-	//get a move
-	getMove();
-
-	//check if legal
-	checkLegal();
-		
-	return 0;
+	std::cout << "checking if move is legal" << std::endl;
+	return true;
 }
