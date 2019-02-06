7c1a3e21f7cbafd04fa26314103a427678ec7787
bsumserLaptop <bsumser@uoregon.edu>
2019-02-06 14:03:32 -0800

    changes to how board is stored in memory

---
+++ assignment4/board.h
@@ -0,0 +1,10 @@
+#ifndef board
+#define board
+
+using namespace std;
+
+class board
+{
+	char board [8][8] = (( ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',),
+						(( ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',),
+}
