b2bea233cece5426a6b9eb1f3d97beb1fcd39645
bsumserLaptop <bsumser@uoregon.edu>
2019-02-06 20:25:20 -0800

    have class with boardArray working, displayBoard is working as well. finishing user input for movee

--- assignment4/main.cpp
+++ assignment4/main.cpp
@@ -3,33 +3,57 @@
 
 class Board
 {
-	char board[8][8] = {(' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '), 
-	                    (' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '), 
-	                    (' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '), 
-	                    (' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '), 
-	                    (' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '), 
-	                    (' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '), 
-	                    (' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '), 
-	                    (' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ')}; 
-					     
-	int row = 1;
-	int column = 1;
+	private:
+		int size = 0;
+	public:
+		char **boardArray; 
+		Board(int);
+
+		int getSize()
+		{
+			return size;
+		}
+
+		int setSize(int i)
+		{
+			size = i;
+		}
 
 };
 
-void displayBoard();
-void getMove();
-bool checkWin();
-bool checkLegal();
+Board::Board(int s)
+{
+
+	//create dynamic array of pointers
+	boardArray = new char* [s];
+
+	size = s;
+
+	//create a row for every pointer
+	for(int i = 0; i < s; i++)
+	{
+		boardArray[i] = new char[s];
+	}
+}
+
+int startBoard(); //gets the size for class instantiation
+void clearBoard(Board &othelloBoard); //sets all the spaces to blank
+void displayBoard(Board &othelloBoard); //displays current state of board
+void getMove(); //receives move from player
+bool checkWin(); //checks if board is in win state
+bool checkLegal(); //checks if move is legal
 
 
 int main()
 {
+	Board othelloBoard(startBoard());
+	clearBoard(othelloBoard);
+
 	//basic game loop
 	do {
 	
 			//display the board
-			displayBoard();
+			displayBoard(othelloBoard);
 			
 			//get a move
 			getMove();
@@ -44,9 +68,35 @@ int main()
 	return 0;
 }
 
-void displayBoard()
+int startBoard()
+{
+	int size;	
+	
+	std::cout << "welcome to othello, please choose board size " << std::endl;
+	std::cin >> size;
+
+	return size;
+}
+
+void clearBoard(Board &othelloBoard)
+{
+	for(int i = 0; i < othelloBoard.getSize(); i++) {
+	   for(int j = 0; j < othelloBoard.getSize(); j++) {
+	   		othelloBoard.boardArray[i][j] = '.';
+	   }
+	}
+}
+
+void displayBoard(Board &othelloBoard)
 {
 		std::cout << "displaying the board" << std::endl;
+
+		for(int i = 0; i < othelloBoard.getSize(); i++) {
+			for(int j = 0; j < othelloBoard.getSize(); j++) {
+					std::cout << othelloBoard.boardArray[i][j];
+			}
+			std::cout << std::endl;
+		}
 }
 
 void getMove()
@@ -58,6 +108,12 @@ void getMove()
 	std::cin >> userRow >> userCol;
 
 	//loop until it recieves proper input
+	while (userRow < 0 || userRow > 8 || userCol < 0 || userCol > 8)
+	{
+		std::cout << "please enter integers between 1 and 8" << std::endl;
+		std::cout << "Please input your move in format row then column" << std::endl;
+		std::cin >> userRow >> userCol;
+	}
 
 	//change row and col to array notation
 	userRow = userRow - 1;
