2e6e7bfef75d24b8fa186ec868e91c734305b980
bsumserLaptop <bsumser@uoregon.edu>
2019-02-22 14:04:10 -0800

    changed inserting element into vector to use push_back() instead of c++ array notation

--- lab7/lab7.cpp
+++ lab7/lab7.cpp
@@ -7,10 +7,11 @@ int main()
 	std::vector<int> v;
 
 	std::default_random_engine generator;
+
 	std::uniform_int_distribution<int> distribution(1,100);
 	for(int i = 0; i < 15; i++) {
 		int number = distribution(generator);
-		v[i] = number;
+		v.push_back(number);
 	}	
 	
 	return 0;
