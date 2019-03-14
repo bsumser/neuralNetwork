f2cb39aa98a0c1da2cd7d1b135c36497a1e62dd3
Brett Sumser <bsumsergmail.com>
2019-03-13 20:29:47 -0700

    added a citation for data used in neural network

--- wine/main.cpp
+++ wine/main.cpp
@@ -82,3 +82,17 @@ int main(int argc, char **argv)
 	sqlite3_close(DB);
 	return(0);
 }
+
+
+/*--------------------Citation for Data used--------------------------------
+P. Cortez, A. Cerdeira, F. Almeida, T. Matos and J. Reis. 
+  Modeling wine preferences by data mining from physicochemical properties.
+  In Decision Support Systems, Elsevier, 47(4):547-553. ISSN: 0167-9236.
+
+  Available at: [@Elsevier] http://dx.doi.org/10.1016/j.dss.2009.05.016
+                [Pre-press (pdf)] http://www3.dsi.uminho.pt/pcortez/winequality09.pdf
+                [bib] http://www3.dsi.uminho.pt/pcortez/dss09.bib
+--------------------Citation for Data used--------------------------------*/
+
+
+/*--------------------Citation for Data used--------------------------------*/
