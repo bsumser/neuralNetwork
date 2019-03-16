a609031c4e413eaf7b39432a987736e24f0042f1
bsumserLaptop <bsumser@uoregon.edu>
2019-03-15 22:06:46 -0700

    writing training data class

---
+++ wine/trainData.h
@@ -0,0 +1,17 @@
+#ifndef TRAINDATA_H
+#define TRAINDATA_H
+#include <vector>
+
+class TrainData
+{
+public:
+
+private:
+	vector<double> inputVals;
+	vector<double> normalVals;
+	vector<double> targetVals;
+
+};
+
+
+#endif
