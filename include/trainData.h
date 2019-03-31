de0ac488bf6c645a6c6d0522795e50875c45400a
bsumserLaptop <bsumser@uoregon.edu>
2019-03-30 17:07:41 -0700

    changes to constructor for trainData class

--- include/trainData.h
+++ include/trainData.h
@@ -5,7 +5,7 @@
 class TrainData
 {
 public:
-	TrainData::TrainData(int row, int col);
+	TrainData::TrainData(int r = 0, int c = 0):row(r), col(c) {}
 	void fillData(); //fills input vals vector with the input data
 	void printInputVals();
 	void printNormalVals();
@@ -13,6 +13,8 @@ public:
 	void normalizeData();
 
 private:
+	int row;
+	int col;
 	vector<double> inputVals;
 	vector<double> normalVals;
 	vector<double> targetVals;
