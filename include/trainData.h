bb929b0059433e5847e6030d1093843293511e17
bsumser <bsumser@gmail.com>
2019-12-27 15:11:18 -0800

    working on constructor to open file and parse the data in

--- include/trainData.h
+++ include/trainData.h
@@ -5,7 +5,11 @@
 class TrainData
 {
 public:
-	TrainData(int r = 0, int c = 0):row(r), col(c) {}
+	TrainData(char *fileArg);
+	void getRow();
+	void setRow(int r);
+	void getCol();
+	void setCol(int c);
 	void fillData(); //fills input vals vector with the input data
 	void printInputVals();
 	void printNormalVals();
@@ -15,6 +19,7 @@ public:
 private:
 	int row;
 	int col;
+	char *fileArg;
 	std::vector<double> inputVals;
 	std::vector<double> normalVals;
 	std::vector<double> targetVals;
--- src/trainData.cpp
+++ src/trainData.cpp
@@ -1,4 +1,31 @@
 #include "../include/trainData.h"
+#include <iostream>
+#include <fstream>
+
+TrainData::TrainData(char *fileArg)
+{
+	std::cout << "setting the input file" << std::endl;
+	std::ifstream inputFile(fileArg);
+}
+void TrainData::getRow()
+{
+
+}
+
+void TrainData::setRow(int r)
+{
+
+}
+
+void TrainData::getCol()
+{
+
+}
+
+void TrainData::setCol(int c)
+{
+
+}
 
 void TrainData::fillData()
 {
