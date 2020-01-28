58fd54f8ccff822f5c12641481c3e1bd8d72e8e2
Brett Sumser <bsumser@gmail.com>
2020-01-27 17:05:39 -0800

    added test printout for file input into trainData class instantiation

--- src/trainData.cpp
+++ src/trainData.cpp
@@ -5,11 +5,19 @@
 TrainData::TrainData(char *fileArg)
 {
 	std::cout << "setting the input file" << std::endl;
-	std::ifstream inputFile(fileArg);
+	std::ifstream inputFile;
+	inputFile.open(fileArg);
+
+	std::cout << "reading from the input file" << std::endl;
+	char data[1000];
+	inputFile >> data;
+
+	std::cout << "writing some data to the screen" << std::endl;
+	std::cout << data << std::endl;
+
+	inputFile.close();
 	//TODO get the number of data items in a line
 	//TODO determine lines in the file
-
-	while ()
 }
 void TrainData::getRow()
 {
