899e37448a3eb01025d58c08617f54a6b8f7ccc6
Brett Sumser <bsumser@gmail.com>
2022-05-09 15:12:53 -0700

    changing argument being sent to training data class

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
 	}
 	
 	//TODO: work on the training data class
-	//TrainData trainData(argv[2]);
+	TrainData trainData(argv[1]);
 
 	if (0 == 1) {
 		//vector to dictate node and layer setup for network
